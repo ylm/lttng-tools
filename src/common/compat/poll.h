c0aee39a9047371e8061c7c74d9438acd2893df3
Yannick Lamarre <ylamarre@efficios.com>
2019-03-13 15:43:45 -0400

    Adapt poll layer behaviour to match the epoll layer

--- src/common/compat/poll.h
+++ src/common/compat/poll.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 - David Goulet <david.goulet@polymtl.ca>
+ * Copyright (C) 2019 - Yannick Lamarre <ylamarre@efficios.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2 only,
